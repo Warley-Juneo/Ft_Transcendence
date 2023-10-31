e361e3a9109cc5eaf36bcbbdca96f9321d1845c2
root <root@DESKTOP-O7CMANF>
2023-10-31 15:06:01 -0300

    refactor perfilt

--- frontend/src/components/ProfilePage/ProfilePage.tsx
+++ frontend/src/components/ProfilePage/ProfilePage.tsx
@@ -4,14 +4,17 @@ import './rank.css'
 
 function ProfileScreen() {
 	return (
-		<div className="row g-0 bg-custon-roxo rounded p-2 h-100">
-			<div className='col-2'>
-				<InformationsUser />
-			</div>
-			<div className='col-10 text-white h-100'>
-				<div className='p-5 h-100'>
+			<div className="row g-0 p-2 bg-custon-roxo rounded h-100">
+				<div className='col-md-3 col-lg-2'>
+					<InformationsUser />
+				</div>
+				<div className='col-md-9 col-lg-10 text-white d-flex flex-column h-100 p-5'>
 					<h2 className='fst-italic ps-3 faixa-amarela'>PARTIDAS RECENTES</h2>
-					<div className='d-flex flex-column overflow-auto h-100' id='MatchHistory'>
+					<div className='overflow-auto' id='MatchHistory'>
+						<MatchHistory />
+						<MatchHistory />
+						<MatchHistory />
+						<MatchHistory />
 						<MatchHistory />
 						<MatchHistory />
 						<MatchHistory />
@@ -27,7 +30,6 @@ function ProfileScreen() {
 					</div>
 				</div>
 			</div>
-		</div>
 	);
 }
 
