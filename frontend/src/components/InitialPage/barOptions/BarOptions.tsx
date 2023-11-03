d31172bd7c5500c4b0d9297d329352953ca34454
root <root@DESKTOP-O7CMANF>
2023-11-03 16:33:12 -0300

    added elements semantics in code

--- frontend/src/components/InitialPage/barOptions/BarOptions.tsx
+++ frontend/src/components/InitialPage/barOptions/BarOptions.tsx
@@ -5,60 +5,53 @@ function BarOptions(props: any) {
 	const navigate = useNavigate();
 
 	// Função para gerar uma classe aleatória entre "animated-button1" e "animated-button12"
-	function randomButtonClass() : string {
+	function randomButtonClass(): string {
 		return 'animated-button' + (Math.floor(Math.random() * 12) + 1);
 	}
 
 	return (
-		<nav className='d-flex bg-custon-roxo' style={{ minHeight: '15vh' }}>
-			<div className='d-flex w-100 my-auto'>
-				<div className='div-bottom-animation'>
-					<span className={randomButtonClass()} onClick={() => navigate("/game/start-game")}>
-						<span></span>
-						<span></span>
-						<span></span>
-						<span></span>
-						Jogar
-					</span>
+		<header>
+			<nav className='d-flex bg-custon-roxo' style={{ minHeight: '15vh' }}>
+				<div className='d-flex w-100 my-auto'>
+					<div className='div-bottom-animation'>
+						<span className={randomButtonClass()} onClick={() => navigate("/game/start-game")}>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							Jogar
+						</span>
+					</div>
+					<div className='div-bottom-animation'>
+						<span className={randomButtonClass()} onClick={() => navigate("/game/chats")}>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							Chats
+						</span>
+					</div>
+					<div className='div-bottom-animation'>
+						<span className={randomButtonClass()} onClick={() => navigate("/game/ranking")}>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							Ranking
+						</span>
+					</div>
+					<div className='div-bottom-animation'>
+						<span className={randomButtonClass()} onClick={() => navigate("/game/profile")}>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							Perfil
+						</span>
+					</div>
 				</div>
-				<div className='div-bottom-animation'>
-					<span className={randomButtonClass()} onClick={() => navigate("/game/chats")}>
-						<span></span>
-						<span></span>
-						<span></span>
-						<span></span>
-						Chats
-					</span>
-				</div>
-				<div className='div-bottom-animation'>
-					<span className={randomButtonClass()} onClick={() => navigate("/game/ranking")}>
-						<span></span>
-						<span></span>
-						<span></span>
-						<span></span>
-						Ranking
-					</span>
-				</div>
-				{/* <div className='div-bottom-animation'>
-					<span className={randomButtonClass()}>
-						<span></span>
-						<span></span>
-						<span></span>
-						<span></span>
-						Histórico
-					</span>
-				</div> */}
-				<div className='div-bottom-animation'>
-					<span className={randomButtonClass()} onClick={() => navigate("/game/profile")}>
-						<span></span>
-						<span></span>
-						<span></span>
-						<span></span>
-						Perfil
-					</span>
-				</div>
-			</div>
-		</nav>
+			</nav>
+		</header>
 	);
 }
 
