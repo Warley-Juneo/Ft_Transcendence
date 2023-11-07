01d29c8931883f6888d5c524e86fb71dc398ce19
root <root@DESKTOP-O7CMANF>
2023-11-07 12:18:41 -0300

    added camp key in Frients List

--- frontend/src/components/InitialPage/MiniPerfil/ListFriends.tsx
+++ frontend/src/components/InitialPage/MiniPerfil/ListFriends.tsx
@@ -4,8 +4,8 @@ import Cookies from 'js-cookie';
 import axios from 'axios';
 import ChatPrivate from '../../ChatsGame/ChatPrivate/ChatPrivate';
 
-function ListFriends() {
-	const	[info, setInfo] = useState([{avatar: "", nickname: ""}]);
+export default function ListFriends() {
+	const	[info, setInfo] = useState([{avatar: "", nickname: "", id: ""}]);
 	const	[chatPrivate, setChatPrivate] = useState(false);
 
 	const showChatPrivate = () => {
@@ -20,6 +20,7 @@ function ListFriends() {
 		})
 		.then((res) => {
 			setInfo(res.data);
+			console.log("res.data: ", res.data);
 		})
 		.catch((err) => {
 			console.log(err);
@@ -30,13 +31,12 @@ function ListFriends() {
 	  axios_connect();
 	}, []);
 
-
 	return (
 		<div className='p-2 text-white overflow-auto'>
 			{chatPrivate && <ChatPrivate />}
 			{info && info.map(friend => {
 				return (
-					<div className='d-flex hover' onClick={showChatPrivate}>
+					<div className='d-flex hover' onClick={showChatPrivate} key={friend.id}>
 					<img className="foto-list-friends" src={friend.avatar} alt='foto' />
 					{StatusOnline(friend.nickname)}
 				</div>
@@ -46,5 +46,3 @@ function ListFriends() {
 		</div>
 	);
 }
-
-export default ListFriends;
