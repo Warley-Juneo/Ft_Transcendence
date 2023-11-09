d829d58032632b6e4a4af0158f6dfe1fc74f6c08
root <root@DESKTOP-O7CMANF>
2023-11-09 13:15:32 -0300

    added new logic game

--- frontend/src/components/GamePage/Game.tsx
+++ frontend/src/components/GamePage/Game.tsx
@@ -1,16 +1,27 @@
 import { useEffect, useState } from "react";
 
+type informationGame = {
+	position: [number, number],
+	direction: [number, number],
+	positionBar: number,
+}
+
 export default function Game() {
-	const [positionEixoYBar, setPositionEixoYBar] = useState(0);
-	const [positionBall, setPositionBall] = useState([0, 1]);
-	const [ballDirection, setBallDirection] = useState([1, 1]);
+	const [infosGame, setInfosGame] = useState<informationGame>({
+		position: [50, 50], direction: [-1, 1], positionBar: 0
+	} as informationGame
+	);
 
 	const handleKeyPress = (e: KeyboardEvent) => {
-		if (e.key === 'ArrowUp') {
-			setPositionEixoYBar((prevPosition) => (prevPosition > 0 ? prevPosition - 2 : prevPosition));
-		} else if (e.key === 'ArrowDown') {
-			setPositionEixoYBar((prevPosition) => (prevPosition < 74 ? prevPosition + 2 : prevPosition));
-		}
+		setInfosGame((prevPosition: informationGame) => {
+			if (e.key === 'ArrowUp') {
+				return { ...prevPosition, positionBar: (prevPosition.positionBar > 0 ? prevPosition.positionBar - 2 : prevPosition.positionBar) };
+			}
+			else if (e.key === 'ArrowDown') {
+				return { ...prevPosition, positionBar: (prevPosition.positionBar < 74 ? prevPosition.positionBar + 2 : prevPosition.positionBar) };
+			}
+			return prevPosition;
+		});
 	};
 
 	useEffect(() => {
@@ -22,34 +33,48 @@ export default function Game() {
 
 	useEffect(() => {
 		const intervalId = setInterval(() => {
-			setPositionBall((prevPosition) => {
-				const [y, x] = prevPosition;
-				const [dirY, dirX] = ballDirection;
+			setInfosGame((prevPosition: informationGame) => {
+				const newInfosball: informationGame = { ...prevPosition };
+				const [y, x] = newInfosball.position;
+				const [dirY, dirX] = newInfosball.direction;
+				const positionEixoYBar = newInfosball.positionBar;
+
 				const newY = y + dirY * 2;
 				const newX = x + dirX * 2;
-				if (newY < 0 || newY > 74)
-					setBallDirection([-dirY, dirX]);
-				if (newX > 74)
-					setBallDirection([dirY, -dirX]);
-				if (newX < 3) {
-					if (newY >= positionEixoYBar && newY <= positionEixoYBar + 26)
-						setBallDirection([dirY, -dirX]);
+
+				if (newY < 0 || newY > 98)
+					newInfosball.direction = [-dirY, dirX];
+				else if (newX > 98)
+					newInfosball.direction = [dirY, -dirX];
+				else if (newX < 4) {
+					if (newY < positionEixoYBar || newY > positionEixoYBar + 26) {
+						clearInterval(intervalId);
+						alert('Você perdeu!');
+					}
 					else
-						setBallDirection([-dirY, dirX]);
+						newInfosball.direction = [-dirY, -dirX];
 				}
-				return [newY, newX];
+				else {
+					newInfosball.position = [newY, newX];
+				}
+				return newInfosball;
 			});
 		}, 75);
-
 		return () => {
 			clearInterval(intervalId);
 		};
-	}, [ballDirection, positionEixoYBar]);
+	}, []);
 
 	return (
 		<div className="bg-custon-roxo h-100 rounded position-relative d-flex">
-			<div className="bg-light position-relative" style={{ top: `${positionEixoYBar}%`, height: '26%', width: '3%' }}></div>
-			<div className="bg-light position-absolute rounded-circle" style={{ height: '30px', width: '30px', top: `${positionBall[0]}%`, left: `${positionBall[1]}%` }}></div>
+			<div
+				className="bg-light position-relative"
+				style={{ top: `${infosGame.positionBar}%`, height: '26%', width: '4%' }}>
+			</div>
+			<div
+				className="bg-light position-absolute rounded-circle"
+				style={{ height: '30px', width: '30px', top: `${infosGame.position[0]}%`, left: `${infosGame.position[1]}%` }}>
+			</div>
 		</div>
 	);
 }
