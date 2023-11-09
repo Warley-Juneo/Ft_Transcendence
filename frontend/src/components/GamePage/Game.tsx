4887e5d0adf413972bf83580871be5c886646f39
root <root@DESKTOP-O7CMANF>
2023-11-09 14:33:28 -0300

    rodando

--- frontend/src/components/GamePage/Game.tsx
+++ frontend/src/components/GamePage/Game.tsx
@@ -4,11 +4,14 @@ type informationGame = {
 	position: [number, number],
 	direction: [number, number],
 	positionBar: number,
+	collisions: number,
+	speed: number
 }
 
 export default function Game() {
 	const [infosGame, setInfosGame] = useState<informationGame>({
-		position: [50, 50], direction: [-1, 1], positionBar: 0
+		position: [20, 20], direction: [-2, 2], positionBar: 0,
+		collisions: 0, speed: 0
 	} as informationGame
 	);
 
@@ -39,20 +42,34 @@ export default function Game() {
 				const [dirY, dirX] = newInfosball.direction;
 				const positionEixoYBar = newInfosball.positionBar;
 
-				const newY = y + dirY * 2;
-				const newX = x + dirX * 2;
+				let newY = y + dirY + (dirY < 0 ? -newInfosball.speed : newInfosball.speed);
+				let newX = x + dirX + (dirX < 0 ? -newInfosball.speed : newInfosball.speed);
 
-				if (newY < 0 || newY > 98)
+
+				if (newY < 0 || newY > 98) {
 					newInfosball.direction = [-dirY, dirX];
-				else if (newX > 98)
+					newY = y + dirY
+					newX = x + dirX
+				}
+				else if (newX > 98) {
 					newInfosball.direction = [dirY, -dirX];
+					newY = y + dirY
+					newX = x + dirX
+				}
 				else if (newX < 4) {
+					newInfosball.collisions++;
+					newInfosball.speed = Math.floor(newInfosball.collisions++ / 3);
 					if (newY < positionEixoYBar || newY > positionEixoYBar + 26) {
 						clearInterval(intervalId);
 						alert('Você perdeu!');
 					}
-					else
+					let randon = Math.floor(Math.random() * 3 + 1);
+					if (randon === 1)
 						newInfosball.direction = [-dirY, -dirX];
+					else if (randon === 2)
+						newInfosball.direction = [dirY, -dirX];
+					else
+						newInfosball.direction = [-dirY, dirX];
 				}
 				else {
 					newInfosball.position = [newY, newX];
@@ -74,6 +91,9 @@ export default function Game() {
 			<div
 				className="bg-light position-absolute rounded-circle"
 				style={{ height: '30px', width: '30px', top: `${infosGame.position[0]}%`, left: `${infosGame.position[1]}%` }}>
+			</div>
+			<div className="d-flex text-white align-items-end justify-content-center w-100">
+				<p>collisions {infosGame.collisions}</p>
 			</div>
 		</div>
 	);
