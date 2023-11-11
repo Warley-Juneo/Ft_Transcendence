e42c4ad8ff5b89a60d8cd1dbe04ed8e64d9e3da8
root <root@DESKTOP-O7CMANF>
2023-11-11 14:02:09 -0300

    added spinner to loading members chats

--- frontend/src/components/ChatsGame/ChatPublic/ChatPublic.tsx
+++ frontend/src/components/ChatsGame/ChatPublic/ChatPublic.tsx
@@ -14,7 +14,11 @@ export default function ChatPublic() {
 	return (
 		<div className="bg-custon-roxo rounded text-white h-100">
 			<div className="row g-0 h-100 p-2">
-				<div className="col-3 border-end">
+				<div className="col-3 d-flex flex-column align-items-center justify-content-center border-end h-100">
+					<p>Carregando lista de mebros...</p>
+					<div className="spinner-border text-primary mt-3 h-25" role="status">
+						<span className="visually-hidden">Loading...</span>
+					</div>
 				</div>
 				<div className="col-9 d-flex flex-column h-100 position-relative">
 					<BarConfigurations openOrClosedConf={openOrClosedConf} />
