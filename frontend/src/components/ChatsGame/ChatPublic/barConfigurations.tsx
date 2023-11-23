90f52df41652f7ddd722742cd80e14a0570d154c
wallas <wagratom@gmail.com>
2023-11-23 12:09:23 -0300

    refactor

--- frontend/src/components/ChatsGame/ChatPublic/barConfigurations.tsx
+++ frontend/src/components/ChatsGame/ChatPublic/barConfigurations.tsx
@@ -1,11 +1,11 @@
 import { AiTwotoneSetting } from 'react-icons/ai';
 import { useParams } from 'react-router-dom';
 
-export default function BarConfigurations({openOrClosedConf}: {openOrClosedConf : () => void}) {
+export default function BarConfigurations({ openOrClosedConf }: { openOrClosedConf: () => void }) {
 	const id = useParams();
 
 	return (
-		<div className="border-bottom d-flex align-items-end" style={{height: "55px"}}>
+		<div className="border-bottom d-flex align-items-end" style={{ height: "55px" }}>
 			<img
 				src="https://i.etsystatic.com/37688069/r/il/d3e600/5143421340/il_600x600.5143421340_sm1f.jpg"
 				alt="bordão sunny one picie"
@@ -14,12 +14,11 @@ export default function BarConfigurations({openOrClosedConf}: {openOrClosedConf 
 			<h3>{id.id}</h3>
 			<AiTwotoneSetting
 				size={25}
-				style={{color: 'white', cursor: 'pointer'}}
+				style={{ color: 'white', cursor: 'pointer' }}
 				className="my-auto ms-auto"
 				onClick={openOrClosedConf}
 			/>
 		</div>
-
 	)
 }
 
