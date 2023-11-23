49502a2bf7989c1a2dbbbcd10c55c35935bc239d
wallas <wagratom@gmail.com>
2023-11-23 12:06:15 -0300

    refactoring profilePage user

--- frontend/src/components/ProfilePage/InformationsUser.tsx
+++ frontend/src/components/ProfilePage/InformationsUser.tsx
@@ -16,6 +16,18 @@ type infosUserPerfil = {
 	_avatar: string;
 }
 
+const rankMappings = [
+	{ max: 5, rank: rank1, borderImg: 'mt-5 borderDivFotoRank1 d-flex', borderWrite: 'd-flex borderWriteRank1 w-100 justify-content-center align-items-center w-100' },
+	{ max: 10, rank: rank2, borderImg: 'mt-5 borderDivFotoRank2 d-flex', borderWrite: 'd-flex borderWriteRank2 w-100 justify-content-center align-items-center pb-5' },
+	{ max: 15, rank: rank3, borderImg: 'mt-5 borderDivFotoRank3 d-flex', borderWrite: 'd-flex borderWriteRank3 w-100 justify-content-center align-items-center w-100' },
+	{ max: 20, rank: rank4, borderImg: 'mt-5 borderDivFotoRank4 d-flex', borderWrite: 'd-flex borderWriteRank4 w-100 justify-content-center align-items-end pb-5 w-100' },
+	{ max: 25, rank: rank4, borderImg: 'mt-5 borderDivFotoRank4 d-flex', borderWrite: 'd-flex borderWriteRank4 w-100 justify-content-center align-items-end pb-5 w-100' },
+	{ max: 30, rank: rank4, borderImg: 'mt-5 borderDivFotoRank4 d-flex', borderWrite: 'd-flex borderWriteRank4 w-100 justify-content-center align-items-end pb-5 w-100' },
+	{ max: 35, rank: rank5, borderImg: 'mt-5 borderDivFotoRank5 d-flex', borderWrite: 'd-flex borderWriteRank4 w-100 justify-content-center align-items-end pb-5 w-100' },
+	{ max: Infinity, rank: rank6, borderImg: 'mt-5 borderDivFotoRank6 d-flex', borderWrite: 'd-flex borderWriteRank4 w-100 justify-content-center align-items-end pb-5 w-100' },
+];
+
+
 type formattingRankUser = {
 	rank: string;
 	borderImg: string;
@@ -23,8 +35,15 @@ type formattingRankUser = {
 }
 
 export default function InformationsUser() {
-	let itemsRank: formattingRankUser = {rank: '', borderImg: '', borderWrite: ''};
-	const [infosUser, setInfosUser] = useState<infosUserPerfil>({} as infosUserPerfil);
+	const [user, setInfosUser] = useState<infosUserPerfil>({} as infosUserPerfil);
+
+	const handleRank = (pointers: number): formattingRankUser => {
+		return rankMappings.find((item) => pointers <= item.max) ||
+			rankMappings[rankMappings.length - 1];
+	}
+
+	let pointers: number = user._wins - user._loses;
+	let maping: formattingRankUser = handleRank(pointers);
 
 	useEffect(() => {
 		axios.get('http://localhost:3000/users/profile ', {
@@ -32,91 +51,33 @@ export default function InformationsUser() {
 				Authorization: Cookies.get('jwtToken'),
 			}
 		})
-		.then((response) => {
-			setInfosUser(response.data);
-		}
-		).catch((error) => {
-			console.log("Error: ", error.response.data);
-		})
+			.then((response) => {
+				setInfosUser(response.data);
+			}
+			).catch((error) => {
+				console.log("Error: ", error.response.data);
+			})
 	}, []);
 
-	const handleRank = (pointers: number) => {
-		if (pointers <= 5) {
-			itemsRank = {
-				borderImg: 'mt-5 borderDivFotoRank1 d-flex',
-				borderWrite: 'd-flex borderWriteRank1 w-100 justify-content-center align-items-center',
-				rank: rank1
-			}
-		}
-		else if (pointers <= 10) {
-			itemsRank = {
-				borderImg: 'mt-5 borderDivFotoRank2 d-flex',
-				borderWrite: 'd-flex borderWriteRank2 w-100 justify-content-center align-items-center pb-5',
-				rank: rank2
-			}
-		}
-		else if (pointers <= 15) {
-			itemsRank = {
-				borderImg: 'mt-5 borderDivFotoRank3 d-flex',
-				borderWrite: 'd-flex borderWriteRank3 w-100 justify-content-center align-items-center w-100',
-				rank: rank3
-			}
-		}
-		else if (pointers <= 20) {
-			itemsRank = {
-				borderImg: 'mt-5 borderDivFotoRank4 d-flex',
-				borderWrite: 'd-flex borderWriteRank4 w-100 justify-content-center align-items-end pb-5 w-100',
-				rank: rank4
-			}
-		}
-		else if (pointers <= 25) {
-			itemsRank = {
-				borderImg: 'mt-5 borderDivFotoRank4 d-flex',
-				borderWrite: 'd-flex borderWriteRank4 w-100 justify-content-center align-items-end pb-5 w-100',
-				rank: rank4
-			}
-		}
-		else if (pointers <= 30) {
-			itemsRank = {
-				borderImg: 'mt-5 borderDivFotoRank4 d-flex',
-				borderWrite: 'd-flex borderWriteRank4 w-100 justify-content-center align-items-end pb-5 w-100',
-				rank: rank4
-			}
-		}
-		else if (pointers <= 35) {
-			itemsRank = {
-				borderImg: 'mt-5 borderDivFotoRank5 d-flex',
-				borderWrite: 'd-flex borderWriteRank4 w-100 justify-content-center align-items-end pb-5 w-100',
-				rank: rank5
-			}
-		}
-		else {
-			itemsRank = {
-				borderImg: 'mt-5 borderDivFotoRank6 d-flex',
-				borderWrite: 'd-flex borderWriteRank4 w-100 justify-content-center align-items-end pb-5 w-100',
-				rank: rank6
-			}
-		}
-	}
 
-	let pointers : number = infosUser._wins - infosUser._loses;
-	handleRank(pointers);
-	const { rank, borderImg, borderWrite } = itemsRank;
+	const { rank, borderImg, borderWrite } = maping;
+	let aux = user._wins + user._draws;
+	let kda: number = aux === 0 ? user._loses : aux / user._loses;
 	return (
 		<div className='text-center text-white'>
 			<div className={borderImg}>
-				<img className='img-fluid rounded-circle m-auto' src={infosUser._avatar} alt='foto' />
+				<img className='img-fluid rounded-circle m-auto' src={user._avatar} alt='foto' />
 			</div>
-			<h2	 className='mt-2 letter-pixel'>{infosUser._nickname}</h2>
+			<h2 className='mt-2 letter-pixel'>{user._nickname}</h2>
 			<div className='d-flex flex-column align-items-center'>
 				<div className='p-2'>
 					<img className='img-fluid' src={rank} alt='' />
 				</div>
-				<div className={borderWrite} style={pointers > 15 ? {height: '200px'} : {height: '150px'}}>
-					<p className='fw-bold me-2'>VT<br></br>{infosUser._wins}</p>
-					<p className='fw-bold me-2'>DR<br></br>{infosUser._loses}</p>
-					<p className='fw-bold me-2'>EPT<br></br>{infosUser._draws}</p>
-					<p className='fw-bold'>KDA<br></br>{infosUser._wins + infosUser._draws / infosUser._loses}</p>
+				<div className={borderWrite} style={pointers > 15 ? { height: '200px' } : { height: '150px' }}>
+					<p className='fw-bold me-2'>VT<br></br>{user._wins}</p>
+					<p className='fw-bold me-2'>DR<br></br>{user._loses}</p>
+					<p className='fw-bold me-2'>EPT<br></br>{user._draws}</p>
+					<p className='fw-bold'>KDA<br></br>{kda}</p >
 				</div>
 			</div>
 		</div>
