7d89d33a4c3ec8e052a05463a2789c87b9c96bd6
wallas <wagratom@gmail.com>
2023-12-04 11:33:17 -0300

    refactor backend

--- backend/src/game/game.controller.ts
+++ backend/src/game/game.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { GameService } from './game.service';
-import { UserMatchesDto } from './dtos/output.dto';
+import { UserMatchDto } from './dtos/output.dto';
 import { InputUserDto } from './dtos/input.dto';
 
 @Controller('game')
@@ -8,7 +8,7 @@ export class GameController {
 	constructor(private readonly gameService: GameService) {}
 
 	@Get('user/match-history')
-	async userMatchs(@Query() dto: InputUserDto): Promise<UserMatchesDto> {
+	async userMatchs(@Query() dto: InputUserDto): Promise<UserMatchDto[]> {
 		return await this.gameService.userMatchs(dto);
 	}
 }
