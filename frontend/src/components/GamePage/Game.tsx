e7de54f5caff6f63c8343366f88f25a18ad317b7
wallas <wagratom@gmail.com>
2023-12-09 13:01:13 -0300

    deslocating nave functional

--- frontend/src/components/GamePage/Game.tsx
+++ frontend/src/components/GamePage/Game.tsx
@@ -1,26 +1,36 @@
 import { useEffect, useRef } from "react";
 import Phaser from "phaser";
 import backgroundSpace from "../../assets/game/backgroundSpace.png";
-import rocket from '../../assets/store/skins/AceIcon.svg';
-import planetaAnel from '../../assets/game/planetaJupter.png';
-import planetaFire from '../../assets/game/planetaFire.png';
-import planetaLua from '../../assets/game/PlanetaLua.png';
+import planetaAnel from '../../assets/game/planets/planetaJupter.png';
+import planetaFire from '../../assets/game/planets/planetaFire.png';
+import planetaLua from '../../assets/game/planets/PlanetaLua.png';
+import naveFrente from '../../assets/game/nave/naveFrente.png';
+import naveCostas from '../../assets/game/nave/naveCostas.png';
+import naveDescendo from '../../assets/game/nave/naveDescendo.png';
+import naveLateral from '../../assets/game/nave/naveLateral.png';
+
 
 export default function Game(): JSX.Element {
 	const gameContainerRef = useRef<HTMLDivElement>(null);
-	let rocketSprite: Phaser.Physics.Arcade.Sprite | undefined;
-	let pntAnel: Phaser.Physics.Arcade.Sprite | undefined;
-	let pntFire: Phaser.Physics.Arcade.Sprite | undefined;
-	let pntLua: Phaser.Physics.Arcade.Sprite | undefined;
+	const nave = useRef<Phaser.Physics.Arcade.Sprite>({} as Phaser.Physics.Arcade.Sprite);
+	const pntAnel = useRef<Phaser.Physics.Arcade.Sprite>({} as Phaser.Physics.Arcade.Sprite)
+	const pntFire = useRef<Phaser.Physics.Arcade.Sprite>({} as Phaser.Physics.Arcade.Sprite)
+	const pntLua = useRef<Phaser.Physics.Arcade.Sprite>({} as Phaser.Physics.Arcade.Sprite)
 
 
 	useEffect(() => {
-		if (!gameContainerRef.current) {
-			return;
-		}
+		if (!gameContainerRef.current) return;
 
 		const containerHeight = gameContainerRef.current?.clientHeight || window.innerHeight; // Altura do container ou da janela
 		const containerWidth = gameContainerRef.current?.clientWidth || window.innerWidth; // Largura do container ou da janela
+
+		const calculeScaleNave = (valueEixoX: number, valueEixoY: number) => {
+			let percentageX = (valueEixoX * 100 / containerWidth) / 1000;
+			percentageX *= percentageX > 0.06 ? 2 : 1;
+			let percentageY = (valueEixoY * 100 / containerHeight) / 1000;
+			percentageY *= percentageY > 0.06 ? 2 : 1;
+			return percentageX + percentageY + 0.1;
+		}
 
 		// Configurações básicas do jogo Phaser
 		const config: Phaser.Types.Core.GameConfig = {
@@ -53,10 +63,14 @@ export default function Game(): JSX.Element {
 		function preload(this: Phaser.Scene) {
 			// Carregue suas imagens aqui, se necessário
 			this.load.image("background", backgroundSpace);
-			this.load.image("rocket", rocket);
 			this.load.image("planetaAnel", planetaAnel);
 			this.load.image("planetaFire", planetaFire);
 			this.load.image("planetaLua", planetaLua);
+			this.load.image('naveFrente', naveFrente);
+			this.load.image('naveCostas', naveCostas);
+			this.load.image('naveDescendo', naveDescendo);
+			this.load.image('naveLateral', naveLateral);
+
 		}
 
 		// Criação de elementos do jogo
@@ -67,49 +81,84 @@ export default function Game(): JSX.Element {
 			background.setScale(containerWidth / background.width, containerHeight / background.height); // Redimensiona o fundo para preencher a tela
 
 			// Phaser.Physics.Arcade.Sprite é o tipo de retorno de this.physics.add.sprite
-			rocketSprite = this.physics.add.sprite(0, containerHeight, "rocket") as Phaser.Physics.Arcade.Sprite;
+			nave.current = this.physics.add.sprite((containerWidth / 2), (containerHeight / 2), "naveFrente") as Phaser.Physics.Arcade.Sprite;
+			nave.current.setVelocityX(100); // Inicializa a velocidade horizontal da nave
+			nave.current.setCollideWorldBounds(true); // Define a colisão com os limites do mundo
+			nave.current.setScale(calculeScaleNave(containerWidth / 2, containerHeight / 2));
 
 			//added planets
-			pntAnel = this.physics.add.sprite((containerWidth * 0.9), (containerHeight * 0.8), "planetaAnel").setImmovable() as Phaser.Physics.Arcade.Sprite
-			pntFire = this.physics.add.sprite((containerWidth * 0.1), (containerHeight * 0.2), "planetaFire").setImmovable() as Phaser.Physics.Arcade.Sprite
-			pntLua = this.physics.add.sprite((containerWidth * 0.7), (containerHeight * 0.2), "planetaLua").setImmovable() as Phaser.Physics.Arcade.Sprite
+			pntFire.current = this.physics.add.sprite((containerWidth * 0.1), (containerHeight * 0.2), "planetaFire").setImmovable() as Phaser.Physics.Arcade.Sprite
+			pntLua.current = this.physics.add.sprite((containerWidth * 0.9), (containerHeight * 0.1), "planetaLua").setImmovable() as Phaser.Physics.Arcade.Sprite
+			pntAnel.current = this.physics.add.sprite((containerWidth * 0.9), (containerHeight * 0.8), "planetaAnel").setImmovable() as Phaser.Physics.Arcade.Sprite
 
-			pntAnel.setName("Anel");
-			pntFire.setName("Fire");
-			pntLua.setName("Lua");
+			pntLua.current.setScale(0.5);
 
-			rocketSprite.setVelocityX(0); // Inicializa a velocidade horizontal da nave
-			this.physics.world.setBounds(0, 0, containerWidth, containerHeight);
-			rocketSprite.setCollideWorldBounds(true);
+			// pntAnel.current.setName("Anel");
+			// pntFire.current.setName("Fire");
+			// pntLua.current.setName("Lua");
+
+			this.physics.world.setBounds(0, 0, containerWidth, containerHeight); // Define os limites do mundo para que a nave não possa sair da tela
 		}
 
 		// Lógica de atualização do jogo
 		function update(this: Phaser.Scene) {
 			const curso = this.input.keyboard?.createCursorKeys(); // event listener para as teclas de seta
 
-			if (!curso || !rocketSprite) { // Se não houver teclas de seta ou a nave não existir, retorne
-				return;
+			if (!curso) return // Se não houver teclas de seta, retorne;
+
+			if (curso.down.isDown || curso.left.isDown || curso.right.isDown || curso.up.isDown) {
+				const {x, y} = nave.current;
+				const inverseSizeX = -(-containerWidth + x)
+				nave.current.setScale(calculeScaleNave(inverseSizeX, y));
+
+				if (curso.left.isDown && curso.down.isDown) {
+					nave.current.setTexture('naveDescendo');
+					nave.current.setFlipX(true);
+					nave.current.setVelocityX(-100);
+					nave.current.setVelocityY(100);
+				}
+				else if (curso.right.isDown && curso.down.isDown) {
+					nave.current.setTexture('naveDescendo');
+					nave.current.setFlipX(false);
+					nave.current.setVelocityX(100);
+					nave.current.setVelocityY(100);
+				}
+				else if (curso.up.isDown && curso.left.isDown) {
+					nave.current.setTexture('naveCostas');
+					nave.current.setFlipX(true);
+					nave.current.setVelocityX(-100);
+					nave.current.setVelocityY(-100);
+				}
+				else if (curso.up.isDown && curso.right.isDown) {
+					nave.current.setTexture('naveCostas');
+					nave.current.setFlipX(false);
+					nave.current.setVelocityX(100);
+					nave.current.setVelocityY(-100);
+				}
+				else if (curso.up.isDown) {
+					nave.current.setTexture('naveCostas');
+					nave.current.setVelocityY(-100);
+				}
+				else if (curso.down.isDown) {
+					nave.current.setTexture('naveFrente');
+					nave.current.setVelocityY(100);
+				}
+				else if (curso.left.isDown) {
+					nave.current.setTexture('naveLateral');
+					nave.current.setFlipX(true);
+					nave.current.setVelocityX(-100);
+				}
+				else if (curso.right.isDown) {
+					nave.current.setTexture('naveLateral');
+					nave.current.setFlipX(false);
+					nave.current.setVelocityX(100);
+				}
+			} else { // Se nenhuma tecla de seta estiver pressionada pare a nave
+				nave.current.setVelocityY(0);
+				nave.current.setVelocityX(0);
 			}
 
-			if (curso.left.isDown) {
-				rocketSprite.setVelocityX(-100);
-			} else if (curso.right.isDown) {
-				rocketSprite.setVelocityX(100);
-			} else {
-				rocketSprite.setVelocityX(0); // Stop horizontal movement when no key is pressed
-			}
-
-			if (curso.up.isDown) {
-				rocketSprite.setVelocityY(-100);
-			} else if (curso.down.isDown) {
-				rocketSprite.setVelocityY(100);
-			} else {
-				rocketSprite.setVelocityY(0); // Stop vertical movement when no key is pressed
-			}
-
-			if (!pntAnel || !pntFire || !pntLua) return;
-
-			this.physics.world.collide(rocketSprite, [pntAnel, pntFire, pntLua], (rocket, planet) => {
+			this.physics.world.collide(nave.current, [pntAnel.current, pntFire.current, pntLua.current], (rocket, planet) => {
 				// Verifica se 'planet' é do tipo Sprite antes de acessar a propriedade 'name'
 				if (planet instanceof Phaser.Physics.Arcade.Sprite && planet.name) {
 					console.log(`A nave colidiu com o planeta: ${planet.name}`);
@@ -124,5 +173,5 @@ export default function Game(): JSX.Element {
 		};
 	}, []); // A dependência vazia garante que isso só seja executado uma vez
 
-	return <div ref={gameContainerRef} className="h-100 rounded" />;
+	return <div ref={gameContainerRef} className="h-100 rounded"></div>;
 }
