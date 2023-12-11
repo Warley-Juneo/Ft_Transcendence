ec0f209e7665ff009aad57070e944c54122ad7d0
wallas <wagratom@gmail.com>
2023-12-11 15:55:52 -0300

    added route to upload to file

--- backend/src/game/game.controller.ts
+++ backend/src/game/game.controller.ts
@@ -1,14 +1,21 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { GameService } from './game.service';
 import { UserMatchDto } from './dtos/output.dto';
-import { InputUserDto } from './dtos/input.dto';
+import { InputUserDto, UploadedFileDto } from './dtos/input.dto';
 
 @Controller('game')
 export class GameController {
-	constructor(private readonly gameService: GameService) {}
+	constructor(private readonly gameService: GameService) { }
 
 	@Get('user/match-history')
 	async userMatchs(@Query() dto: InputUserDto): Promise<UserMatchDto[]> {
 		return await this.gameService.userMatchs(dto);
 	}
+
+	@Post('user/photo')
+	async userPhoto(dto: UploadedFileDto): Promise<string> {
+		console.log(dto.content);
+		return await this.gameService.userPhoto(dto.content, dto.nickname);  // Corrigir aqui para passar o arquivo para o serviço
+	}
+
 }
--- backend/src/game/game.service.ts
+++ backend/src/game/game.service.ts
@@ -2,61 +2,81 @@ import { Injectable } from '@nestjs/common';
 import { GameRepository } from './game.repository';
 import { UserMatchDto } from './dtos/output.dto';
 import { DisconnectDto, InputUserDto } from './dtos/input.dto';
+import * as fs from 'fs';
+import path from 'path';
 
 @Injectable()
 export class GameService {
-  constructor(private readonly gameRepository: GameRepository) {}
+	constructor(private readonly gameRepository: GameRepository) { }
 
-  async getStatusMatch(player_1, player_2) {
-	if (player_1 === player_2) {
-		return "EMPATE";
-	} else if (player_1 > player_2) {
-		return "VITÓRIA";
-	} else {
-		return "DERROTA";
+	async getStatusMatch(player_1, player_2) {
+		if (player_1 === player_2) {
+			return "EMPATE";
+		} else if (player_1 > player_2) {
+			return "VITÓRIA";
+		} else {
+			return "DERROTA";
+		}
 	}
-  }
 
-  async userMatchs(dto: InputUserDto): Promise<UserMatchDto[]> {
-    let matches = await this.gameRepository.userMatchs(dto.user_id);
-	let outpuDto: UserMatchDto[] = [];
+	async userMatchs(dto: InputUserDto): Promise<UserMatchDto[]> {
+		let matches = await this.gameRepository.userMatchs(dto.user_id);
+		let outpuDto: UserMatchDto[] = [];
 
-    for(const obj of matches) {
-      let match_dto = new UserMatchDto;
-      if (dto.user_id == obj.player_1.id) {
-        match_dto.id = obj.id;
-        match_dto.opponent = obj.player_2.nickname;
-        match_dto.opponent_avatar = obj.player_2.avatar;
-        match_dto.opponent_score = obj.score_p2;
-        match_dto.my_score = obj.score_p1;
-        match_dto.status = await this.getStatusMatch(obj.score_p1, obj.score_p2);
-      }
-      else {
-        match_dto.id = obj.id;
-        match_dto.opponent = obj.player_1.nickname;
-        match_dto.opponent_avatar = obj.player_1.avatar;
-        match_dto.opponent_score = obj.score_p1;
-        match_dto.my_score = obj.score_p2;
-        match_dto.status = await this.getStatusMatch(obj.score_p2, obj.score_p1);
-      }
-      outpuDto.push(match_dto);
-    }
-    return outpuDto;
-  }
+		for (const obj of matches) {
+			let match_dto = new UserMatchDto;
+			if (dto.user_id == obj.player_1.id) {
+				match_dto.id = obj.id;
+				match_dto.opponent = obj.player_2.nickname;
+				match_dto.opponent_avatar = obj.player_2.avatar;
+				match_dto.opponent_score = obj.score_p2;
+				match_dto.my_score = obj.score_p1;
+				match_dto.status = await this.getStatusMatch(obj.score_p1, obj.score_p2);
+			}
+			else {
+				match_dto.id = obj.id;
+				match_dto.opponent = obj.player_1.nickname;
+				match_dto.opponent_avatar = obj.player_1.avatar;
+				match_dto.opponent_score = obj.score_p1;
+				match_dto.my_score = obj.score_p2;
+				match_dto.status = await this.getStatusMatch(obj.score_p2, obj.score_p1);
+			}
+			outpuDto.push(match_dto);
+		}
+		return outpuDto;
+	}
 
-  async numberOfUserMatchWins(userId: string): Promise<number> {
-    return this.gameRepository.numberOfUserMatchWins(userId);
-  }
+	async userPhoto(file: any, nickname: string): Promise<string> {
 
-  async numberOfUserMatchLoses(userId: string): Promise<number> {
-    return this.gameRepository.numberOfUserMatchLoses(userId);
-  }
+		try {
+			const uploadFolder = path.resolve(__dirname, '..', 'uploads');
+			if (!fs.existsSync(uploadFolder)) {
+				fs.mkdirSync(uploadFolder); // Crie a pasta 'uploads', se ela não existir
+			}
+			const extension = path.extname(file.originalname);
+			const FilePath = path.resolve(uploadFolder, `${nickname}${extension}`);
 
-  async numberOfUserMatchDraws(userId: string): Promise<number> {
-    return this.gameRepository.numberOfUserMatchDraws(userId);
-  }
+			fs.writeFileSync(FilePath, file.buffer);
+			return `Foto do usuário salva com sucesso em: ${FilePath}`;
+		} catch (error) {
+			console.error('Erro ao salvar a foto:', error);
+			throw new Error('Erro ao salvar a foto'); // Lançar uma exceção para o cliente
+		}
+	}
 
-  async disconnect(data: DisconnectDto): Promise<void> {
-	return this.gameRepository.updateStatusUser(data);
-  }
+	async numberOfUserMatchWins(userId: string): Promise<number> {
+		return this.gameRepository.numberOfUserMatchWins(userId);
+	}
+
+	async numberOfUserMatchLoses(userId: string): Promise<number> {
+		return this.gameRepository.numberOfUserMatchLoses(userId);
+	}
+
+	async numberOfUserMatchDraws(userId: string): Promise<number> {
+		return this.gameRepository.numberOfUserMatchDraws(userId);
+	}
+
+	async disconnect(data: DisconnectDto): Promise<void> {
+		return this.gameRepository.updateStatusUser(data);
+	}
 }
