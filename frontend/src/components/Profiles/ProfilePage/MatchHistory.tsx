db936dd76fc5d3632529641453be80181cbdbda9
wallas <wagratom@gmail.com>
2023-12-13 12:12:47 -0300

    remove cod duplicate to verify overflow-y in profile

--- frontend/src/components/Profiles/ProfilePage/MatchHistory.tsx
+++ frontend/src/components/Profiles/ProfilePage/MatchHistory.tsx
@@ -56,42 +56,6 @@ export default function MatchHistory({ userId }: { userId: string }) {
 					</div>
 				)
 			})}
-				{matchHistory.map((match: MatchHistoryType) => {
-				return (
-					<div className='d-flex p-2 justify-content-between hover text-center' key={match.id}>
-						<div style={{ height: '4rem' }}>
-							<img className='img-fluid rounded-circle h-100' src={match.opponent_avatar} alt={`avatar do ${match.opponent} `} />
-						</div>
-						<div className='fs-5 col-3'>
-							<p>Oponente</p>
-							<p>{match.opponent}</p>
-						</div>
-						<div className='fs-5 fw-bold col-2'>
-							<p>HISTORY</p>
-							<p>{`${match.opponent_score} X ${match.my_score}`}</p>
-						</div>
-						<p className='letter-pixel fs-1 derrota'>{match.status}</p>
-					</div>
-				)
-			})}
-				{matchHistory.map((match: MatchHistoryType) => {
-				return (
-					<div className='d-flex p-2 justify-content-between hover text-center' key={match.id}>
-						<div style={{ height: '4rem' }}>
-							<img className='img-fluid rounded-circle h-100' src={match.opponent_avatar} alt={`avatar do ${match.opponent} `} />
-						</div>
-						<div className='fs-5 col-3'>
-							<p>Oponente</p>
-							<p>{match.opponent}</p>
-						</div>
-						<div className='fs-5 fw-bold col-2'>
-							<p>HISTORY</p>
-							<p>{`${match.opponent_score} X ${match.my_score}`}</p>
-						</div>
-						<p className='letter-pixel fs-1 derrota'>{match.status}</p>
-					</div>
-				)
-			})}
 		</div>
 	);
 }
