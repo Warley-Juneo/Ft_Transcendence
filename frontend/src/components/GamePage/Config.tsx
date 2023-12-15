8684d101667346c7b3bd089ad77ce7ce94775241
wallas <wagratom@gmail.com>
2023-12-15 11:11:10 -0300

    refactor code

--- frontend/src/components/GamePage/Config.tsx
+++ frontend/src/components/GamePage/Config.tsx
@@ -1,71 +1,78 @@
-import Phaser, { Scene } from 'phaser';
+import Phaser from 'phaser';
 import test from '../../assets/game/barrafuncional.png';
 
 export class CustomScene extends Phaser.Scene {
-  paddle1: Phaser.Physics.Arcade.Sprite | undefined;
-  paddle2: Phaser.Physics.Arcade.Sprite | undefined;
-  paddle1UpKey: Phaser.Input.Keyboard.Key | undefined;
-  paddle1DownKey: Phaser.Input.Keyboard.Key | undefined;;
-  paddle2UpKey: Phaser.Input.Keyboard.Key | undefined;;
-  paddle2DownKey: Phaser.Input.Keyboard.Key | undefined;;
-  ball: Phaser.Physics.Arcade.Sprite | undefined;
+	paddle1: Phaser.Physics.Arcade.Sprite;
+	paddle2: Phaser.Physics.Arcade.Sprite;
+	paddle1UpKey: Phaser.Input.Keyboard.Key;
+	paddle1DownKey: Phaser.Input.Keyboard.Key;;
+	paddle2UpKey: Phaser.Input.Keyboard.Key;;
+	paddle2DownKey: Phaser.Input.Keyboard.Key;;
+	ball: Phaser.Physics.Arcade.Sprite;
 
-  preload() {
-    this.load.image('paddle', test);
-    this.load.image('ball', 'caminho/para/imagem/da/bola.png');
-  }
+	constructor() {
+		super('CustomScene');
+		this.paddle1 = {} as Phaser.Physics.Arcade.Sprite;
+		this.paddle2 = {} as Phaser.Physics.Arcade.Sprite;
+		this.paddle1UpKey = {} as Phaser.Input.Keyboard.Key;
+		this.paddle1DownKey = {} as Phaser.Input.Keyboard.Key;
+		this.paddle2UpKey = {} as Phaser.Input.Keyboard.Key;
+		this.paddle2DownKey = {} as Phaser.Input.Keyboard.Key;
+		this.ball = {} as Phaser.Physics.Arcade.Sprite;
+	}
 
-  create() {
-    this.add.text(this.scale.width / 2, 10, 'Space Pong', { font: '16px Courier', color: '#00ff00' });
-    this.paddle1 = this.physics.add.sprite(50, this.scale.height / 2, 'paddle')
-      .setImmovable(true)
-      .setScale(0.1);
-    this.paddle2 = this.physics.add.sprite(this.scale.width - 50, this.scale.height / 2, 'paddle')
-      .setImmovable(true)
-      .setScale(0.1);
+	preload() {
+		this.load.image('paddle', test);
+		this.load.image('ball', 'caminho/para/imagem/da/bola.png');
+	}
 
-    if (this.input && this.input.keyboard) {
-      this.paddle1UpKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-      this.paddle1DownKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-      this.paddle2UpKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-      this.paddle2DownKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
-    }
-    this.ball = this.physics.add.sprite(this.scale.width / 2, this.scale.height / 2, 'ball');
+	create() {
+		this.add.text(this.scale.width / 2, 10, 'Space Pong', { font: '16px Courier', color: '#00ff00' });
 
-    this.ball.setVelocity(300, 0);
-    this.ball.setCollideWorldBounds(true);
-    this.physics.world.setBoundsCollision(false, false, true, true);
-    
-    this.paddle1.setCollideWorldBounds(true);
-    this.paddle2.setCollideWorldBounds(true);
+		this.paddle1 = this.physics.add.sprite(50, this.scale.height / 2, 'paddle')
+			.setImmovable(true)
+			.setScale(0.1);
+		this.paddle2 = this.physics.add.sprite(this.scale.width - 50, this.scale.height / 2, 'paddle')
+			.setImmovable(true)
+			.setScale(0.1);
 
-    this.physics.add.collider(this.ball, this.paddle1, this.ballHitPaddle, undefined, this);
-    this.physics.add.collider(this.ball, this.paddle2, this.ballHitPaddle, undefined, this);
-  }
+		if (this.input && this.input.keyboard) {
+			this.paddle1UpKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+			this.paddle1DownKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+			this.paddle2UpKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+			this.paddle2DownKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+		}
 
-  ballHitPaddle(ball: any, paddle: any) {
-  
-  }
-  
-  update(this: CustomScene ) {
-    if (!this.ball) return;
-    if (!this.paddle1 || !this.paddle2) return;
+		this.ball = this.physics.add.sprite(this.scale.width / 2, this.scale.height / 2, 'ball');
 
-    if (this.ball.x < 0 || this.ball.x > this.scale.width) {
-      console.log('A bola atingiu a borda lateral. Reiniciando o jogo.');
-      this.scene.restart();
-    }
-    if (this.paddle1UpKey && this.paddle1UpKey.isDown) {
-      console.log('Paddle 1 subindo')
-      this.paddle1.setVelocityY(-150);
-    } else if (this.paddle1DownKey && this.paddle1DownKey.isDown) {
-      this.paddle1.setVelocityY(150);
-    }
-    if (this.paddle2UpKey && this.paddle2UpKey.isDown) {
-      this.paddle2.setVelocityY(-150);
-    } else if (this.paddle2DownKey && this.paddle2DownKey.isDown) {
-      this.paddle2.setVelocityY(150);
-    }
-  }
+		this.ball.setVelocity(300, 0);
+		this.ball.setCollideWorldBounds(true);
+		this.physics.world.setBoundsCollision(false, false, true, true);
+
+		this.paddle1.setCollideWorldBounds(true);
+		this.paddle2.setCollideWorldBounds(true);
+
+		this.physics.add.collider(this.ball, this.paddle1, this.ballHitPaddle, undefined, this);
+		this.physics.add.collider(this.ball, this.paddle2, this.ballHitPaddle, undefined, this);
+	}
+
+	ballHitPaddle(ball: any, paddle: any) {
+
+	}
+
+	update(this: CustomScene) {
+		if (!this.ball) return;
+
+		if (!this.paddle1 || !this.paddle2) return;
+
+		if (this.ball.x < 0 || this.ball.x > this.scale.width) this.scene.restart();
+		if (this.paddle1UpKey.isDown) this.paddle1.setVelocityY(-150);
+		else if (this.paddle1DownKey.isDown) this.paddle1.setVelocityY(150);
+		else this.paddle1.setVelocityY(0)
+
+		if (this.paddle2UpKey.isDown) this.paddle2.setVelocityY(-150);
+		else if (this.paddle2DownKey.isDown) this.paddle2.setVelocityY(150);
+		else this.paddle2.setVelocityY(0)
+	}
 }
 
--- frontend/src/components/GamePage/Game.tsx
+++ frontend/src/components/GamePage/Game.tsx
@@ -2,36 +2,37 @@ import { CustomScene } from "./Config";
 import React, { useEffect } from 'react';
 
 const PongGame = () => {
-  const gameContainerRef = React.useRef<HTMLDivElement>(null);
+	const gameContainerRef = React.useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (!gameContainerRef.current) return;
-    const config = {
-      type: Phaser.AUTO,
-      parent: gameContainerRef.current,
-      
-      width:  gameContainerRef.current?.clientWidth || window.innerWidth,
-      height: gameContainerRef.current?.clientHeight || window.innerHeight,
-      physics: {
-        default: 'arcade',
-        arcade: {
-          gravity: { y: 0 },
-          debug: true
-        }
-      },
-      scene: [CustomScene]
-    };
+	useEffect(() => {
+		if (!gameContainerRef.current) return;
 
-    const game = new Phaser.Game(config);
+		const config = {
+			type: Phaser.AUTO,
+			parent: gameContainerRef.current,
 
-    return () => {
-      if (game) {
-        game.destroy(true);
-      }
-    };
-  }, []);
+			width: gameContainerRef.current?.clientWidth || window.innerWidth,
+			height: gameContainerRef.current?.clientHeight || window.innerHeight,
+			physics: {
+				default: 'arcade',
+				arcade: {
+					gravity: { y: 0 },
+					debug: true
+				}
+			},
+			scene: [CustomScene]
+		};
 
-  return <div ref={gameContainerRef} className="h-100 rounded bg-custon-roxo" />;
+		const game = new Phaser.Game(config);
+
+		return () => {
+			if (game) {
+				game.destroy(true);
+			}
+		};
+	}, []);
+
+	return <div ref={gameContainerRef} className="h-100 rounded bg-custon-roxo" />;
 
 };
 
