6cc20de8199cadd6a98d2a553647002e6edd2dcb
Warley-Juneo <warleyjuneo111@gmail.com>
2023-12-15 10:29:07 -0300

    feat: push push

--- frontend/src/components/GamePage/Config.tsx
+++ frontend/src/components/GamePage/Config.tsx
@@ -1,36 +1,28 @@
-import React, { useEffect } from 'react';
-import Phaser from 'phaser';
-import pong from '../../assets/game/R.png'
+import Phaser, { Scene } from 'phaser';
+import test from '../../assets/game/barrafuncional.png';
 
 export class CustomScene extends Phaser.Scene {
-  paddle1: any;
-  paddle2: any;
-  paddle1UpKey: any;
-  paddle1DownKey: any;
-  paddle2UpKey: any;
-  paddle2DownKey: any;
-  ball: any;
+  paddle1: Phaser.Physics.Arcade.Sprite | undefined;
+  paddle2: Phaser.Physics.Arcade.Sprite | undefined;
+  paddle1UpKey: Phaser.Input.Keyboard.Key | undefined;
+  paddle1DownKey: Phaser.Input.Keyboard.Key | undefined;;
+  paddle2UpKey: Phaser.Input.Keyboard.Key | undefined;;
+  paddle2DownKey: Phaser.Input.Keyboard.Key | undefined;;
+  ball: Phaser.Physics.Arcade.Sprite | undefined;
 
   preload() {
-    this.load.image('paddle', pong);
+    this.load.image('paddle', test);
     this.load.image('ball', 'caminho/para/imagem/da/bola.png');
   }
 
   create() {
+    this.add.text(this.scale.width / 2, 10, 'Space Pong', { font: '16px Courier', color: '#00ff00' });
     this.paddle1 = this.physics.add.sprite(50, this.scale.height / 2, 'paddle')
       .setImmovable(true)
-      .setScale(0.5);
+      .setScale(0.1);
     this.paddle2 = this.physics.add.sprite(this.scale.width - 50, this.scale.height / 2, 'paddle')
       .setImmovable(true)
-      .setScale(0,5);
-    this.ball = this.physics.add.sprite(this.scale.width / 2, this.scale.height / 2, 'ball');
-
-    this.ball.setVelocity(300, 0);
-    this.ball.setBounce(1);
-    this.ball.setCollideWorldBounds(true);
-
-    this.physics.add.collider(this.ball, this.paddle1);
-    this.physics.add.collider(this.ball, this.paddle2);
+      .setScale(0.1);
 
     if (this.input && this.input.keyboard) {
       this.paddle1UpKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
@@ -38,39 +30,41 @@ export class CustomScene extends Phaser.Scene {
       this.paddle2UpKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
       this.paddle2DownKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
     }
+    this.ball = this.physics.add.sprite(this.scale.width / 2, this.scale.height / 2, 'ball');
+
+    this.ball.setVelocity(300, 0);
+    this.ball.setCollideWorldBounds(true);
+    this.physics.world.setBoundsCollision(false, false, true, true);
+    
+    this.paddle1.setCollideWorldBounds(true);
+    this.paddle2.setCollideWorldBounds(true);
+
+    this.physics.add.collider(this.ball, this.paddle1, this.ballHitPaddle, undefined, this);
+    this.physics.add.collider(this.ball, this.paddle2, this.ballHitPaddle, undefined, this);
   }
 
-  update() {
+  ballHitPaddle(ball: any, paddle: any) {
+  
+  }
+  
+  update(this: CustomScene ) {
+    if (!this.ball) return;
+    if (!this.paddle1 || !this.paddle2) return;
+
+    if (this.ball.x < 0 || this.ball.x > this.scale.width) {
+      console.log('A bola atingiu a borda lateral. Reiniciando o jogo.');
+      this.scene.restart();
+    }
     if (this.paddle1UpKey && this.paddle1UpKey.isDown) {
+      console.log('Paddle 1 subindo')
       this.paddle1.setVelocityY(-150);
     } else if (this.paddle1DownKey && this.paddle1DownKey.isDown) {
       this.paddle1.setVelocityY(150);
     }
-
     if (this.paddle2UpKey && this.paddle2UpKey.isDown) {
       this.paddle2.setVelocityY(-150);
     } else if (this.paddle2DownKey && this.paddle2DownKey.isDown) {
       this.paddle2.setVelocityY(150);
-    }
-
-    if (this.physics.world.collide(this.ball, this.paddle1)) {
-      // Calcula a diferença entre o centro da raquete e o ponto de colisão
-      let diff = this.ball.y - this.paddle1.y;
-      // Se a bola colide no centro da raquete, não altera o ângulo
-      if (diff === 0) {
-        diff = 0.5;
-      }
-      // Ajusta a velocidade vertical da bola com base na diferença
-      this.ball.setVelocityY(300, diff);
-    }
-  
-    // Verifica se a bola está colidindo com a raquete do jogador 2
-    if (this.physics.world.collide(this.ball, this.paddle2)) {
-      let diff = this.ball.y - this.paddle2.y;
-      if (diff === 0) {
-        diff = 0.5;
-      }
-      this.ball.setVelocityY(300, diff);
     }
   }
 }
--- frontend/src/components/GamePage/Game.tsx
+++ frontend/src/components/GamePage/Game.tsx
@@ -2,15 +2,21 @@ import { CustomScene } from "./Config";
 import React, { useEffect } from 'react';
 
 const PongGame = () => {
+  const gameContainerRef = React.useRef<HTMLDivElement>(null);
+
   useEffect(() => {
+    if (!gameContainerRef.current) return;
     const config = {
       type: Phaser.AUTO,
-      width: 1000,
-      height: 600,
+      parent: gameContainerRef.current,
+      
+      width:  gameContainerRef.current?.clientWidth || window.innerWidth,
+      height: gameContainerRef.current?.clientHeight || window.innerHeight,
       physics: {
         default: 'arcade',
         arcade: {
-          gravity: { y: 0 }
+          gravity: { y: 0 },
+          debug: true
         }
       },
       scene: [CustomScene]
@@ -25,7 +31,8 @@ const PongGame = () => {
     };
   }, []);
 
-  return <div id="phaser-game-container" />;
+  return <div ref={gameContainerRef} className="h-100 rounded bg-custon-roxo" />;
+
 };
 
 export default PongGame;
