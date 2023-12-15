1d774ce58036e1889a70781d2b5a1740d8687b47
wallas <wagratom@gmail.com>
2023-12-15 13:26:30 -0300

    game added

--- frontend/src/components/GamePage/Config.tsx
+++ frontend/src/components/GamePage/Config.tsx
@@ -9,6 +9,7 @@ export class CustomScene extends Phaser.Scene {
 	paddle2UpKey: Phaser.Input.Keyboard.Key;;
 	paddle2DownKey: Phaser.Input.Keyboard.Key;;
 	ball: Phaser.Physics.Arcade.Sprite;
+	dataBallGame: { x: number, y: number, lastpaddle: string };
 
 	constructor() {
 		super('CustomScene');
@@ -19,6 +20,7 @@ export class CustomScene extends Phaser.Scene {
 		this.paddle2UpKey = {} as Phaser.Input.Keyboard.Key;
 		this.paddle2DownKey = {} as Phaser.Input.Keyboard.Key;
 		this.ball = {} as Phaser.Physics.Arcade.Sprite;
+		this.dataBallGame = { x: 200, y: 0, lastpaddle: '' };
 	}
 
 	preload() {
@@ -45,27 +47,45 @@ export class CustomScene extends Phaser.Scene {
 
 		this.ball = this.physics.add.sprite(this.scale.width / 2, this.scale.height / 2, 'ball');
 
-		this.ball.setVelocity(300, 0);
+		this.ball.setVelocity(200, 0);
 		this.ball.setCollideWorldBounds(true);
-		this.physics.world.setBoundsCollision(false, false, true, true);
 
 		this.paddle1.setCollideWorldBounds(true);
 		this.paddle2.setCollideWorldBounds(true);
 
-		this.physics.add.collider(this.ball, this.paddle1, this.ballHitPaddle, undefined, this);
-		this.physics.add.collider(this.ball, this.paddle2, this.ballHitPaddle, undefined, this);
+		this.physics.add.collider(this.ball, this.paddle1, () => { this.ballHitPaddle('paddle1') });
+		this.physics.add.collider(this.ball, this.paddle2, () => { this.ballHitPaddle('paddle2') })
 	}
 
-	ballHitPaddle(ball: any, paddle: any) {
+	ballHitPaddle(paddle: string) {
+		let { lastpaddle, x, y } = this.dataBallGame;
 
+		if (lastpaddle === paddle) return;
+
+		this.dataBallGame.lastpaddle = paddle;
+		x = x < 0 ? (x - 20) * -1 : (x + 20) * -1;
+		// y = y < 0 ? (y - 20) * -1 : (y + 20) * -1;
+
+		this.dataBallGame.x = x;
+		this.dataBallGame.y = Phaser.Math.Between(-200, 200);
+
+		this.ball.setVelocityX(this.dataBallGame.x);
+		this.ball.setVelocityY(this.dataBallGame.y);
 	}
 
 	update(this: CustomScene) {
 		if (!this.ball) return;
-
+		if (!this.ball.body) return
 		if (!this.paddle1 || !this.paddle2) return;
 
-		if (this.ball.x < 0 || this.ball.x > this.scale.width) this.scene.restart();
+		if (this.ball.x <= 17 || this.ball.x >= this.scale.width - 17) this.scene.restart();
+
+		if (this.ball.y <= 17 || this.ball.y >= this.scale.height-17) {
+			this.ball.setVelocityY(-this.dataBallGame.y);
+			if (this.ball.y <= 17) this.ball.y = 18;
+			if (this.ball.y >= this.scale.height-17) this.ball.y = this.scale.height-18;
+		}
+
 		if (this.paddle1UpKey.isDown) this.paddle1.setVelocityY(-150);
 		else if (this.paddle1DownKey.isDown) this.paddle1.setVelocityY(150);
 		else this.paddle1.setVelocityY(0)
