4655fa495a863c976d88a810332734fbfe042071
wallas <wagratom@gmail.com>
2023-12-16 16:10:51 -0300

    alter route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import './index.css';
 import { Login } from './components/LoginPage/Login';
 import InicialPage from './components/InitialPage/InitialPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ChatPublic from './components/ChatsGame/ChatPublic/ChatPublic';
 import LoginFake from './components/LoginPage/LoginFake';
 import Game from './components/GamePage/Game/Game';
 import GamePong from './components/GamePage/GamePong/GamePong';
@@ -15,8 +14,8 @@ export default function App() {
 					<Route path="/" element={<Login />} />
 					<Route path="/fake" element={<LoginFake />} />
 					<Route path="/game/" element={<InicialPage />}>
-						{/* <Route path="gameLobby/" element={<Game />} /> */}
-						<Route path="gameLobby/" element={<GamePong />} />
+						<Route path="gameLobby/" element={<Game />} />
+						{/* <Route path="gameLobby/" element={<GamePong />} /> */}
 						<Route path="pong/" element={<GamePong />} />
 						{/* <Route path="chats/:chatName" element={<ChatPublic />} /> */}
 					</Route>
