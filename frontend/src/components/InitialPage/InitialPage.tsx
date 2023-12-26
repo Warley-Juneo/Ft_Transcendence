17d41110bfb3984e4bf5a6bfc2342d318e7dcdd6
wallas <wagratom@gmail.com>
2023-12-26 13:42:50 -0300

    pro fausto teste

--- frontend/src/components/InitialPage/InitialPage.tsx
+++ frontend/src/components/InitialPage/InitialPage.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import axios from "axios";
 import Cookies from "js-cookie";
-import { UserData, t_dataUser } from './Contexts/Contexts';
+import { UserData, socket, t_dataUser } from './Contexts/Contexts';
 
 export default function InicialPage() {
 	let timeForNewRequestAxios: number = 10000;
@@ -15,27 +15,33 @@ export default function InicialPage() {
 		twoFA: false,
 	});
 
+	function setStatusOnline(id: string) {
+		let aux = {
+			user_id: id,
+			is_active: true,
+		}
+		socket.emit('check-status', aux);
+	}
+
 	function getInfoUser() {
 		axios.get('http://localhost:3000/landing-page', {
 			headers: {
 				Authorization: Cookies.get('jwtToken'),
 			}, timeout: 5000
-		})
-			.then((res) => {
-				if (!res.data.avatar) {
-					res.data.avatar = "https://i.pinimg.com/originals/e7/3a/7c/e73a7c77c2430210674a0c0627d9ca76.jpg";
-				}
-				setGetInfoUser(res.data);
-				console.log(res.data);
-			})
-			.catch(() => {
-				timeout++
-				if (timeout === 5) {
-					alert("O servidor esta indisponivel no momento, tente novamente mais tarde.");
-					timeForNewRequestAxios = 60000;
-				}
-				setTimeout(getInfoUser, timeForNewRequestAxios);
-			});
+		}).then((res) => {
+			if (!res.data.avatar) {
+				res.data.avatar = "https://i.pinimg.com/originals/e7/3a/7c/e73a7c77c2430210674a0c0627d9ca76.jpg";
+			}
+			setGetInfoUser(res.data);
+			setStatusOnline(res.data.id);
+		}).catch(() => {
+			timeout++
+			if (timeout === 5) {
+				alert("O servidor esta indisponivel no momento, tente novamente mais tarde.");
+				timeForNewRequestAxios = 60000;
+			}
+			setTimeout(getInfoUser, timeForNewRequestAxios);
+		});
 	}
 
 	useEffect(() => {
--- frontend/src/components/LoginPage/Login.tsx
+++ frontend/src/components/LoginPage/Login.tsx
@@ -2,12 +2,11 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import { AuthLogin } from './authLogin';
-import { Navigate } from 'react-router';
+import { useNavigate } from 'react-router';
 import './style.css';
 
 export function Login() {
-
-	const [auth, setAuth] = useState(false);
+	const navigate = useNavigate();
 
 	//ACCESS BACKEND AFTER GET THE CODE AT
 	async function axios_connect(): Promise<void> {
@@ -16,24 +15,30 @@ export function Login() {
 		if (code) {
 			await axios.post('http://localhost:3000/auth', {
 				authCode: code,
+			}).then((response) => {
+				if (response.status === 201) {
+					Cookies.set('jwtToken', response.data._access_token);// set expires time
+					Cookies.set('email', response.data._email);
+					navigate('/game')
+				}
+				else {
+					console.log("RENDERIZAR PAGINA LOGIN INFORMANDO O ERRO");
+				}
 			})
-				.then((response) => {
-					if (response.status === 201) {
-						Cookies.set('jwtToken', response.data._access_token);// set expires time
-						Cookies.set('email', response.data._email);
-						setAuth(true);
-					}
-					else {
-						console.log("RENDERIZAR PAGINA LOGIN INFORMANDO O ERRO");
-					}
-				})
 		}
 	}
 
 	//THIS FUNCTION IS EXECUTED EVERY TIME THE PAGE IS LOADED
 	useEffect(() => {
-		axios_connect();
+		if (Cookies.get('jwtToken')) {
+			console.log("entrei")
+			navigate('/game')
+		}
+		else {
+			axios_connect();
+		}
 	}, []);
+
 
 	return (
 		<div className="login  ">
@@ -45,7 +50,6 @@ export function Login() {
 					</div>
 				</form>
 			</div>
-			{auth && <Navigate to='/game/' replace={true} />}
 		</div>
 	);
 }
