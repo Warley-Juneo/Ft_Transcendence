379a7e6e514637b2fc1034617cfe7d56effe7a17
wallas <wagratom@gmail.com>
2024-01-19 11:46:32 -0300

    move route create chat to webwesocket

--- frontend/src/components/PublicChatsPage/PublicChats.tsx
+++ frontend/src/components/PublicChatsPage/PublicChats.tsx
@@ -1,4 +1,4 @@
-import React, { SetStateAction, useEffect, useState } from 'react';
+import React, { SetStateAction, useContext, useEffect, useState } from 'react';
 import bgChats from '../../assets/game/planets/backgrounds/bgChats.jpg'
 import ScreenCreateNewChat from './ScreemCreateNewChat';
 import BarOptions from './BarOptions';
@@ -8,6 +8,7 @@ import axios from 'axios';
 import './PublicChats.css';
 import ButtonClosed from '../GamePage/Game/ButtonClosed';
 import ChatPublic from '../ChatsGame/ChatPublic/ChatPublic';
+import { UserData, socket } from '../InitialPage/Contexts/Contexts';
 
 export type t_chat = {
 	id: string;
@@ -24,12 +25,13 @@ type propsPageChats = {
 }
 
 export default function PageChats(props: propsPageChats) {
+	const user_id = useContext(UserData).user.id;
 	const [chatList, setChatList] = useState<t_chat[]>([]);
 	const [showCreateChat, setShowCreateChat] = useState(false);
 	const [selectedChat, setSelectedChat] = useState({ click: false, chatName: '' });
+	//TODO: Adicionar um variavel para ver se ele esta no privado ou publico
 
 	const getListPublicChats = () => {
-		console.log("Vou pegar a lista de getListPublicChats")
 		axios.get(`${process.env.REACT_APP_HOST_URL}/chatroom/find-all-public`, {
 			headers: {
 				Authorization: Cookies.get("jwtToken"),
@@ -76,31 +78,32 @@ export default function PageChats(props: propsPageChats) {
 
 	function createNewChat(form: FormData) {
 		setShowCreateChat(false);
-		const dataPost = {
+		const obj = {
+			my_id: user_id,
 			name: form.get('nameChat'),
 			type: 'public',
 			password: form.get('passwordChat'),
 			photoUrl: "https://photografos.com.br/wp-content/uploads/2020/09/fotografia-para-perfil.jpg",
 		}
 
-		if (form.get('privateChat') === 'private') dataPost.type = 'private'
-		else if (form.get('protectChat') === 'protected') dataPost.type = 'protected'
+		if (form.get('privateChat') === 'private') obj.type = 'private'
+		else if (form.get('protectChat') === 'protected') obj.type = 'protected'
 
-		axios.post(`${process.env.REACT_APP_HOST_URL}/chatroom/create-group`,
-			dataPost, {
-			headers: {
-				Authorization: Cookies.get('jwtToken'),
-			}
-		}).then((res) => {
-			setChatList(res.data.chatrooms)
-		}).catch((err) => {
-			console.log(err)
-		})
+		socket.emit('create-group', obj);
 	}
 
 	useEffect(() => {
 		getListPublicChats()
 	}, [])
+
+	useEffect(() => {
+		socket.on("creatChat", (data: any) => {
+			getListPublicChats()
+		})
+		return () => {
+			socket.off("creatChat")
+		}
+	})
 
 	const cssDivChats: React.CSSProperties = {
 		backgroundImage: `url(${bgChats})`,
