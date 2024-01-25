67a70eb4a3ff4c295069ef6b965c8e1fcffdc843
wallas <wagratom@gmail.com>
2024-01-25 15:51:36 -0300

    adde moute to kick

--- frontend/src/components/Profiles/MiniProfile/options.tsx
+++ frontend/src/components/Profiles/MiniProfile/options.tsx
@@ -2,10 +2,13 @@
 import { FaPeopleGroup } from "react-icons/fa6";
 import { FaUserFriends } from 'react-icons/fa';
 import { MdPersonRemoveAlt1, MdPersonAddAlt1 } from "react-icons/md";
+import { MdBlock } from "react-icons/md";
 
-import React, { useState } from 'react';
+
+import React, { useContext, useState } from 'react';
 import axios from 'axios';
 import Cookies from 'js-cookie';
+import { UserData, socket } from "../../InitialPage/Contexts/Contexts";
 
 const URLS_MiniPerfilPlayers = {
 	'personal': `${process.env.REACT_APP_HOST_URL}/users/friends`,
@@ -15,6 +18,9 @@ const URLS_MiniPerfilPlayers = {
 function Options({ getPlayers }: { getPlayers: (route: string) => void }) {
 	const [showADDFriend, setShowAddFriend] = useState(false);
 	const [showDLTFriend, setShowDLTFriend] = useState(false);
+	const [showBLOKFriend, setShowBLOKFriend] = useState(false);
+	const userData = useContext(UserData);
+	//todo criar um componente botão pra nao precisar ficar criando varios use stage
 
 
 	function addNewFriend(event: React.KeyboardEvent<HTMLInputElement>) {
@@ -49,6 +55,16 @@ function Options({ getPlayers }: { getPlayers: (route: string) => void }) {
 		}
 	}
 
+	function BlockUser(event: React.KeyboardEvent<HTMLInputElement>) {
+		if (event.key === 'Enter') {
+			let obj = {
+				my_nickname: userData.user.nickname,
+				other_nickname: event.currentTarget.value
+			}
+			socket.emit("direct-block", obj)
+		}
+	}
+
 	function returnInput(func: (event: React.KeyboardEvent<HTMLInputElement>) => void) {
 		return (
 			<div className='rounded'>
@@ -72,12 +88,24 @@ function Options({ getPlayers }: { getPlayers: (route: string) => void }) {
 				<p className='fw-bold'>Social</p>
 				<div className='d-flex justify-content-end w-100 options'>
 					{/* TODO: ADDED Function to delete friend */}
+					<MdBlock
+						style={styleButton}
+						size={30}
+						onClick={
+							() => {
+								setShowAddFriend(false)
+								setShowDLTFriend(false)
+								setShowBLOKFriend(!showBLOKFriend)
+							}
+						}
+					/>
 					<MdPersonRemoveAlt1
 						style={styleButton}
 						size={30}
 						onClick={() => {
-							setShowDLTFriend(!showDLTFriend)
+							setShowBLOKFriend(false)
 							setShowAddFriend(false)
+							setShowDLTFriend(!showDLTFriend)
 						}}
 					/>
 					<MdPersonAddAlt1
@@ -85,23 +113,26 @@ function Options({ getPlayers }: { getPlayers: (route: string) => void }) {
 						size={30}
 						onClick={
 							() => {
-								setShowAddFriend(!showADDFriend)
+								setShowBLOKFriend(false)
 								setShowDLTFriend(false)
+								setShowAddFriend(!showADDFriend)
 							}
 						}
 					/>
 					<FaUserFriends
 						style={styleButton}
-						size={30} onClick={() => { getPlayers(URLS_MiniPerfilPlayers.personal) }}
+						size={30} onClick={() => getPlayers(URLS_MiniPerfilPlayers.personal)}
 					/>
 					<FaPeopleGroup
 						style={styleButton}
 						size={30} onClick={() => getPlayers(URLS_MiniPerfilPlayers.Global)}
 					/>
+
 				</div>
 			</div>
 			{showADDFriend ? returnInput(addNewFriend) : null}
 			{showDLTFriend ? returnInput(DeleteFriend) : null}
+			{showBLOKFriend ? returnInput(BlockUser) : null}
 		</div>
 	)
 }
