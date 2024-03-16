02a6201a9ebaa041655aa0590bf022c719d0db75
FaustoFaggion <faustofaggion@hotmail.com>
2024-03-16 09:35:58 -0300

    update avatar functions

--- frontend/src/components/Profiles/ProfilePage/Perfil/Image.tsx
+++ frontend/src/components/Profiles/ProfilePage/Perfil/Image.tsx
@@ -4,7 +4,8 @@ export default function ProfilePhoto(props: propsImageProfile): JSX.Element {
 	return (
 		<div className="h-100 d-flex flex-column">
 			<div className={props.borderImg} style={{height: '95%'}} >
-				<img className='img-fluid rounded-circle m-auto h-100' src={props.avatar} alt='foto' />
+				<img className='img-fluid rounded-circle m-auto h-100' src={`data:image/jpeg;base64, ${props.avatar}`}
+ alt='foto' />
 			</div>
 			<p className='letter-pixel fs-1'>{props.nickname}</p>
 		</div>
